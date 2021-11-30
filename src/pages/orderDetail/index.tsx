39fc4472c6e53da1fdb216106f6b09fbd83cdc5b
gangyunhu <hugangyun@openseas.com.cn>
2021-11-30 16:58:05 +0800

    feat:订单详情页订单待付款页面构建

--- src/pages/orderDetail/index.tsx
+++ src/pages/orderDetail/index.tsx
@@ -3,32 +3,22 @@ import { useStore } from '@/store/context'
 import { observer } from 'mobx-react-lite'
 import React, { useState } from 'react'
 import './index.less'
-import { CountDown } from 'react-vant';
+import { CountDown,Icon } from 'react-vant';
+import integralIcon from '@/assets/img/integral_icon.png'
+import wecharIcon from '@/assets/img/wechar_icon@3x.png'
 
 const OrderDetailPage = observer(() => {
-  const [getResult, setGetResult] = useState('')
-  const store = useStore()
-
-  const testGetRequest = async () => {
-    const res = await common.getUser()
-    setGetResult(JSON.stringify(res))
-  }
-
-  const changeUserName = () => {
-    console.log(store)
-    store.userStore.setUserName('asds')
-  }
-
+ 
   return (
     <div className="Order-container">
         <div className="order-count">
         <CountDown 
-          time={30 * 60 * 60 * 3000} 
+          time={30 * 60 * 60 * 2000} 
           format="剩 DD 天 HH:mm:ss" 
         />
         </div>
         <div className="order-main">
-            <div className="order-call_card">
+            <div className="contact_card">
                 <div className="card-l">
                     <img src="https://img01.yzcdn.cn/vant/logo.png" />
                 </div>
@@ -41,11 +31,11 @@ const OrderDetailPage = observer(() => {
                     </div>
                 </div>
                 <div className="card-r">
-                <img src="https://img01.yzcdn.cn/vant/logo.png" />
+                    <img src={wecharIcon} />
                 </div>
             </div>
 
-          <div className="order-info_card">
+          <div className="preview_card">
               <div className="info-content">
                   <div className="info-l">
                     <img src="https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00264-2075.jpg" />
@@ -63,11 +53,68 @@ const OrderDetailPage = observer(() => {
                       </div>
                   </div>
               </div>
+              <div className="info-integral rv-hairline--bottom">
+                  <div className="integral-title hairline--icon">
+                    <Icon size="4vw" className="integra-icon"  name={integralIcon} />
+                    <span>积分</span>
+                    </div>
+                  <div className="integral-instruction">
+                  使用¥340<span>-¥340</span>
+                  </div>
+              </div>
+
+              <div className="info-discounts">
+                  <div className="discounts-title hairline--icon" >优惠<Icon className="discounts-icon" name="question-o" /></div>
+                  <div className="discounts-instruction">
+                    <div className="instruction-l">
+                    已优惠<span>¥200</span>
+                    </div>
+                    <div className="instruction-r">
+                    共计¥<span>5798</span>
+                    </div>
+                  </div>
+              </div>
           </div>
 
+
+          <div className="payment_card">
+                <div className="payment-name">支付方式</div>
+                <div className="payment-select">微信<Icon color="#999999" name="arrow" /></div>
+          </div>
+
+          <div className="indent_card">
+              <ul className="indent-ul">
+                <li className="indent-li">
+                    <div className="indent-li_l">
+                      订单编号
+                    </div>
+                    <div className="indent-li_r">1234 1234 1234 1234</div>
+                    <div className="indent-li_copy">复制</div>
+                </li>
+                <li className="indent-li">
+                  <div className="indent-li_l">下单时间</div>
+                  <div className="indent-li_r">2021/10/19 18:43:20</div>
+                </li>
+              </ul>
+          </div>
+
+          <div className="backthat_card">
+                <div className="backthat-name">退改说明</div>
+                <div className="backthat-select">
+                  <Icon color="#999999" name="arrow" />
+                </div>
+          </div>
         </div>
         <div className="order-action">
-            <div className="order-action-item"></div>
+            <div className="action-main">
+                <div className="action-l">
+                  <div className="action-total"><span>¥</span> 8596</div>
+                  <div className="action-dis">已优惠2198</div>
+                </div>
+                <div className="action-r">
+                  <div className="btn-pay">立即付款</div>
+                </div>
+            </div>
         </div>
     </div>
   )
