e91f135a149eb73a01081b8b21ef699e8545b173
fangmingdong <fangmingdong0224@gmail.com>
2021-12-02 13:33:39 +0800

    fix: Auto commit by fe-cli.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = ({ file }) => {
-  const designWidth = file.toString().indexOf("vant") > -1 ? 375 : 750; //根据设计图设置大小适配vant-react
+  const designWidth = file.toString().indexOf('vant') > -1 ? 375 : 750 //根据设计图设置大小适配vant-react
   return {
     plugins: [
       require('autoprefixer'),
