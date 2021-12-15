f9269ffc9b3b811c47fffd2449bc7cd3432fffeb
gangyunhu <hugangyun@openseas.com.cn>
2021-12-15 18:20:10 +0800

    feat：已知时间日历增加查看更多按钮

--- src/components/orderDetail/backthatCard/index.tsx
+++ src/components/orderDetail/backthatCard/index.tsx
@@ -1,12 +1,12 @@
-import React, { useState,FC } from 'react'
+import React, { useState, FC } from 'react'
 
-import { Icon } from 'react-vant';
+import { Icon } from 'react-vant'
 import './index.less'
 /**
  * 退改说明卡片
  */
 
-const BackThatCard:FC = (props) => {
+const BackThatCard: FC = (props) => {
   return (
     <div className="backthat_card">
       <div className="backthat-name">退改说明</div>
