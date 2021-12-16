2c1b8ac1bff63ffd6f0aed518641fc8463db7e1d
gangyunhu <hugangyun@openseas.com.cn>
2021-12-16 10:37:48 +0800

    feat:申请退款新增 阻截弹窗

--- src/pages/applySaleService/indexRefund.tsx
+++ src/pages/applySaleService/indexRefund.tsx
@@ -1,15 +1,9 @@
-import React, { useState,FC } from 'react'
+import React, { useState, FC } from 'react'
 
 import GoodsCard from '@/components/orderDetail/goodsCard'
 import PreferCard from '@/components/orderDetail/preferCard'
 import BackCard from '@/components/orderDetail/backthatCard'
 import ApplyRefundCard from '@/components/applySale/applyRefundCard'
-import RefundReasonCard from '@/components/applySale/refundReasonCard'
-import RefundPieceCard from '@/components/applySale/refundPieceCard'
-import RefundAmountCard from '@/components/applySale/refundAmount'
-import CancelTripCard from '@/components/applySale/cancelTripCard'
-import RefundInstrucCard from '@/components/applySale/refundInstrucCard'
-
 
 import './index.less'
 
@@ -17,29 +11,22 @@ import './index.less'
  * 申请退款入口
  * type 1 申请退款
  */
-const IndexRefund: FC = (props:any) => {
-
-  console.log('object :>> ', props);
+const IndexRefund: FC = (props: any) => {
+  console.log('object :>> ', props)
   // const {location:{search}} = props;
   // const {type} = qs.parse(search.slice(1))
   return (
     <div className="refund-container">
-        <div className="refund-main">
-          <div className="refund-card">
-              <GoodsCard/>
-              <PreferCard/>
-          </div>
-          <ApplyRefundCard/>
-          <BackCard/>
-          
+      <div className="refund-main">
+        <div className="refund-card">
+          <GoodsCard />
+          <PreferCard />
         </div>
-        
+        <ApplyRefundCard />
+        <BackCard />
+      </div>
     </div>
   )
 }
 
 export default IndexRefund
-
-
-
-
