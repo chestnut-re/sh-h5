0de6b663c3d1cb68e1897443c9bd41d572a27de7
张瑞 <zhangrui@openseas.com.cn>
2021-12-29 22:37:43 +0800

    商品详情价格归位

--- src/pages/goodsDetail/index.tsx
+++ src/pages/goodsDetail/index.tsx
@@ -10,7 +10,10 @@ import 'swiper/css/navigation'
 import GoodsDetailTemplate from './components/GoodsDetailTemplate'
 import Panel from './components/Panel'
 import { Controller } from 'swiper'
-
+import 'swiper/css'
+import 'swiper/css/effect-fade'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 import './index.less'
 
 // {"code":"200","msg":"成功","data":{"id":1473837487611928576,"goodsPriceId":1473837487616122880,"goodsName":"欧洲11国双日游","goodsNickName":"跑断腿","personCurrentPrice":1,"personCostPrice":1,"personMarkPrice":1,"childCurrentPrice":1,"childCostPrice":1,"childMarkPrice":1,"stock":99,"days":7,"goodsDetailStart":{"pageTemplate":"首尾页模板","backgroundImage":null,"backgroundImgs":["https://s1.ax1x.com/2021/12/10/o5hkUs.png"],"sellingTag":[],"submitOrderImg":null},"goodsDetailPage":[{"pageTemplate":"详情页1模板","detailTitle":"详情1","detailDesc":"详情1简介","backgroundImage":null,"backgroundImgs":["https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg9.51tietu.net%2Fpic%2F2019-091106%2F1eqn3t2og0k1eqn3t2og0k.jpg&refer=http%3A%2F%2Fimg9.51tietu.net&app=2002&size=f9999","https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg9.51tietu.net%2Fpic%2F2019-091116%2Fzwk1xx25efgzwk1xx25efg.jpg&refer=http%3A%2F%2Fimg9.51tietu.net&app=2002&size=f9999"],"price":9999,"priceColour":"#000000","submitOrderImg":"提交订单图片url"}],"goodsDetailEnd":{"pageTemplate":"首尾页模板","backgroundImage":null,"backgroundImgs":["https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg9.51tietu.net%2Fpic%2F2019-091106%2F1eqn3t2og0k1eqn3t2og0k.jpg&refer=http%3A%2F%2Fimg9.51tietu.net&app=2002&size=f9999","https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg9.51tietu.net%2Fpic%2F2019-091116%2Fzwk1xx25efgzwk1xx25efg.jpg&refer=http%3A%2F%2Fimg9.51tietu.net&app=2002&size=f9999"],"sellingTag":[],"currentPriceColour":"#fff","markPriceColour":"#999","currentPriceTextColour":null,"markPriceTextColour":null,"submitOrderImg":null}}}
@@ -68,6 +71,7 @@ const GoodsDetailPage: React.FC = () => {
         <Swiper
           className="swiper"
           direction="vertical"
+          effect={'fade'}
           loop={false}
           pagination={{
             clickable: true,
