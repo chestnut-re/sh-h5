56430ccaca08c541488c4a056781a899088e88d3
fangmingdong <fangmingdong0224@gmail.com>
2021-12-29 13:11:52 +0800

    fix: Auto commit by fe-cli.

--- src/pages/goodsDetail/components/GSwiper/index.tsx
+++ src/pages/goodsDetail/components/GSwiper/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { PageTemplateKey } from '../../utils'
 import './index.less'
+
+interface Props {
+  imgs: string[]
+  templateKey: PageTemplateKey
+}
 
 /**
  * 轮播
  */
-const GSwiper: React.FC = () => {
-  return <div className='GSwiper__root'></div>
+const GSwiper: React.FC<Props> = ({ imgs }) => {
+  return (
+    <div className="GSwiper__root">
+      {imgs && (
+        <Swiper effect={'cards'} grabCursor={true} className="mySwiper">
+          {imgs.map((item, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <img src={item} />
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      )}
+    </div>
+  )
 }
 
 export default GSwiper
--- src/pages/goodsDetail/components/GoodsDetailTemplate/index.tsx
+++ src/pages/goodsDetail/components/GoodsDetailTemplate/index.tsx
@@ -6,6 +6,7 @@ import SubmitBtn from '../SubmitBtn'
 import Title from '../Title'
 import Label from '../Label'
 import './index.less'
+import GSwiper from '../GSwiper'
 
 interface Props {
   templateKey: PageTemplateKey
@@ -19,7 +20,7 @@ interface Props {
  */
 const GoodsDetailTemplate: React.FC<Props> = ({ data, title, templateKey, makeOrder }) => {
   return (
-    <div className='GoodsDetailTemplate__root'>
+    <div className="GoodsDetailTemplate__root">
       {/* 背景图片 */}
       <BgImg img={data?.backgroundImage} />
       {/* 标题图 */}
@@ -30,6 +31,8 @@ const GoodsDetailTemplate: React.FC<Props> = ({ data, title, templateKey, makeOr
       <Label img={data?.goodsTypeTagImage} templateKey={templateKey} />
       {/* 价格图 */}
       <Price img={data?.priceImage} templateKey={templateKey} />
+      {/* 轮播图 */}
+      <GSwiper imgs={data?.contentImages} templateKey={templateKey} />
     </div>
   )
 }
