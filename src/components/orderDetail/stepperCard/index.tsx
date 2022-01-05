48ac8e2c6de49c174f37b08cf3b5c67b9354eab7
gangyunhu <hugangyun@openseas.com.cn>
2022-01-05 18:52:49 +0800

    feat：kuchun

--- src/components/orderDetail/stepperCard/index.tsx
+++ src/components/orderDetail/stepperCard/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, FC } from 'react'
 
-import { ConfigProvider, Icon,Toast, Stepper } from 'react-vant'
+import { ConfigProvider, Icon, Toast, Stepper } from 'react-vant'
 import integralIcon from '@/assets/img/integral_icon.png'
 import questionIcon from '@/assets/img/question_icon@3x.png'
 import './index.less'
@@ -18,10 +18,16 @@ interface StepType {
   handleStepper: (val) => void
   selectTime: any
   priceSet: any
-  tokenAmountNum:number; 
+  tokenAmountNum: number
 }
 
-const StepperCard: FC<StepType> = ({selectTime:{pointsDeduction,stock}, priceSet,tokenAmountNum,handleStepper,handleDiscounts}) => {
+const StepperCard: FC<StepType> = ({
+  selectTime: { pointsDeduction, stock },
+  priceSet,
+  tokenAmountNum,
+  handleStepper,
+  handleDiscounts,
+}) => {
   //成人数量
   const [adultNum, setAdultNum] = useState(1)
   //儿童数量
@@ -29,47 +35,54 @@ const StepperCard: FC<StepType> = ({selectTime:{pointsDeduction,stock}, priceSet
   //积分使用量
   const [inteNum, setInteNum] = useState(0)
 
-  //库存数量adultStock成人可选库存量 childStock：儿童可选库存量
-  const [stockdata,setStockdata] = useState({
-    adultStock:0,
-    childStock:0,
-  })
-  useEffect(()=>{
-    setStockdata({
-      adultStock:stock,
-      childStock:stock,
-    })
-  },[stock])
-  useEffect(()=>{
-    setStockdata({
-      adultStock:stock-childNum,
-      childStock:stock-adultNum,
-    })
-  },[stock,adultNum,childNum])
+  
 
   //手动输入失去焦点判断当前值是否大于库存 大于库存设置为最大值
-
+  const setGrownNumBlur = (e, type) => {
+    console.log('val :>> ', e)
+    let inputVal = e.target.value
+    if (type == 1) {
+      const MacStockNum = stock - childNum
+      if (inputVal > MacStockNum) {
+        inputVal = MacStockNum
+        setAdultNum(MacStockNum)
+        Toast(`最多只能买${MacStockNum}件`)
+      }
+    } else if (type == 2) {
+      const MacStockNum = stock - adultNum
+      if (inputVal > MacStockNum) {
+        inputVal = MacStockNum
+        setChildrenVal(MacStockNum)
+        Toast(`最多只能买${MacStockNum}件`)
+      }
+    } else if (type == 3) {
+      //积分数据判断
+      if (inputVal > pointsDeduction) {
+        inputVal = pointsDeduction
+        setInteNum(pointsDeduction)
+        Toast(`最多只能使用${pointsDeduction}积分`)
+      }
+    }
+  }
 
   const setGrownNumValue = (val) => {
-    setStockdata((v)=>{
-      return {
-        ...v,
-        childStock:stock-v.adultStock,
-      }
-    })
-    setAdultNum(val)
-   
+    const AduStock = stock - childNum;
+    if (AduStock - val <= 0) {
+      Toast(`预定总数最多${AduStock}份`)
+      setAdultNum(AduStock)
+    } else {
+      setAdultNum(val)
+    }
   }
- 
+
   const setChildrenValue = (val) => {
-    console.log('val :>> ', val)
-    setChildrenVal(val)
-    setStockdata((v)=>{
-      return {
-        ...v,
-        adultStock:stock-v.childStock,
-      }
-    })
+    const ChildAduStock = stock - adultNum;
+    if (ChildAduStock - val <= 0) {
+      Toast(`预定总数最多${ChildAduStock}份`)
+      setChildrenVal(ChildAduStock)
+    } else {
+      setChildrenVal(val)
+    }
   }
   const setIntegralNumValue = (val) => {
     console.log('val :>> ', val)
@@ -79,12 +92,12 @@ const StepperCard: FC<StepType> = ({selectTime:{pointsDeduction,stock}, priceSet
     handleDiscounts()
   }
   //处理用户输入位数过多导致总价显示变形
-const beforeChangeValue = (val)=>{
-  // if (val>stock) {
-  //   return false
-  // }
-  return true
-}
+  const beforeChangeValue = (val) => {
+    // if (val>stock) {
+    //   return false
+    // }
+    return true
+  }
   useEffect(() => {
     handleStepper({
       adultNum: adultNum && adultNum > 0 ? adultNum : 1, //成人数量
@@ -110,7 +123,9 @@ const beforeChangeValue = (val)=>{
                   integer={true}
                   inputWidth="9.6vw"
                   buttonSize="5.6vw"
+                  beforeChange={(val) => beforeChangeValue(val)}
                   onChange={(val) => setGrownNumValue(val)}
+                  onBlur={(val) => setGrownNumBlur(val, 1)}
                 />
               </ConfigProvider>
             </div>
@@ -128,31 +143,33 @@ const beforeChangeValue = (val)=>{
                   integer={true}
                   inputWidth="9.6vw"
                   buttonSize="5.6vw"
+                  beforeChange={(val) => beforeChangeValue(val)}
                   onChange={(val) => setChildrenValue(val)}
+                  onBlur={(val) => setGrownNumBlur(val, 2)}
                 />
               </ConfigProvider>
             </div>
           </li>
-          {pointsDeduction&&tokenAmountNum>0 ? (
+          {pointsDeduction && tokenAmountNum > 0 ? (
             <li className="step-boxli">
               <div className="step-name hairline--icon">
                 <Icon size="4vw" className="integra-icon" name={integralIcon} />
                 <span>金豆</span>
-                <span className="name-subtitle">共{(tokenAmountNum)/RMB_CON}</span>
+                <span className="name-subtitle">共{tokenAmountNum / RMB_CON}</span>
               </div>
               <div className="step-content">
                 <ConfigProvider themeVars={themeVars}>
                   <Stepper
-                    disabled={(pointsDeduction/RMB_CON)<1?true:false}
+                    disabled={pointsDeduction / RMB_CON < 1 ? true : false}
                     value={inteNum}
                     min="0"
-                    max={pointsDeduction/RMB_CON}
+                    max={pointsDeduction / RMB_CON}
                     step="1"
                     longPress={false}
                     integer={true}
                     inputWidth="9.6vw"
                     buttonSize="5.6vw"
-                    beforeChange={(val)=>beforeChangeValue(val)}
+                    beforeChange={(val) => beforeChangeValue(val)}
                     onChange={(val) => setIntegralNumValue(val)}
                   />
                 </ConfigProvider>
@@ -162,31 +179,33 @@ const beforeChangeValue = (val)=>{
         </ul>
       </div>
 
-      {pointsDeduction&&tokenAmountNum>0 ? (
+      {pointsDeduction && tokenAmountNum > 0 ? (
         <div className="info-integral rv-hairline--bottom">
           <div className="integral-instruction">
-            此订单最多可用{pointsDeduction/RMB_CON}金豆抵<span>¥{pointsDeduction/RMB_CON}</span>
+            此订单最多可用{pointsDeduction / RMB_CON}金豆抵<span>¥{pointsDeduction / RMB_CON}</span>
           </div>
         </div>
       ) : null}
 
-     {priceSet?.preferPrice>0?( <div className="info-discounts">
-        <div
-          className="discounts-title hairline--icon"
-          onClick={() => {
-            getExamine()
-          }}
-        >
-          优惠
-          <Icon className="discounts-icon" name={questionIcon} />
-        </div>
-        <div className="discounts-instruction">
-          <div className="instruction-l">
-            已优惠<b>¥</b>
-            <span>{priceSet?.preferPrice}</span>
+      {priceSet?.preferPrice > 0 ? (
+        <div className="info-discounts">
+          <div
+            className="discounts-title hairline--icon"
+            onClick={() => {
+              getExamine()
+            }}
+          >
+            优惠
+            <Icon className="discounts-icon" name={questionIcon} />
+          </div>
+          <div className="discounts-instruction">
+            <div className="instruction-l">
+              已优惠<b>¥</b>
+              <span>{priceSet?.preferPrice}</span>
+            </div>
           </div>
         </div>
-      </div>):null}
+      ) : null}
     </div>
   )
 }
