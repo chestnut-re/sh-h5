112cbb5900eeb9b91104611021c39b751669fff6
gangyunhu <hugangyun@openseas.com.cn>
2022-01-11 18:33:38 +0800

    update

--- src/pages/applySaleService/index.tsx
+++ src/pages/applySaleService/index.tsx
@@ -1,10 +1,12 @@
-import React, { useState, FC } from 'react'
-
+import React, { useState,useEffect, FC } from 'react'
+import { useLocation } from 'react-router-dom'
 import qs from 'query-string'
-
+import {RefundApi} from '../../service/RefundApply';
+import {OrderApi} from '@/service/OrderDetailApi';
 import ApplyRefund from './applyRefund'
 import RefundProcess from './refundProcess'
 import IndexRefund from './indexRefund'
+
 import './index.less'
 
 /**
@@ -14,17 +16,30 @@ import './index.less'
  * type 2 退款成功 退款进行中 退款失败
  *
  */
-const RefundIndexPage: FC = (props: any) => {
-  console.log('object :>> ', props)
-  const {
-    location: { search },
-  } = props
-  const { type } = qs.parse(search.slice(1))
+const RefundIndexPage: FC = () => {
+  const { search } = useLocation()
+  const { type,orderId } = qs.parse(search.slice(1))
+
+  const [orderDetail, setOrderDetail] = useState(null)
+
+  useEffect(() => {
+    OrderApi.orderdetail({orderId}).then((res) => {
+        console.log('res 订单详情:>> ', res);
+        const {code,data} = res;
+        if (code==="200"&&data) {
+          setOrderDetail(data)
+        }
+    }).catch((err) => {
+        console.log('err :>> ', err);
+    });
+  }, [])
+
+
   return (
     <div className="container">
-      {type === '0' && <IndexRefund />}
-      {type === '1' && <ApplyRefund />}
-      {type === '2' && <RefundProcess />}
+      {type === '0' && <IndexRefund orderInfo={orderDetail} />}
+      {type === '1' && <ApplyRefund/>}
+      {type === '2' && <RefundProcess/>}
     </div>
   )
 }
--- src/pages/applySaleService/indexRefund.tsx
+++ src/pages/applySaleService/indexRefund.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FC } from 'react'
+import React, { useState,useEffect, FC } from 'react'
 
 import GoodsCard from '@/components/orderDetail/goodsCard'
 import PreferCard from '@/components/orderDetail/preferCard'
@@ -7,19 +7,33 @@ import ApplyRefundCard from '@/components/applySale/applyRefundCard'
 
 import './index.less'
 
+interface IndexRefundType{
+  orderInfo:any
+}
+
 /**
  * 申请退款入口
  * type 1 申请退款
  */
-const IndexRefund: FC = (props: any) => {
-  console.log('object :>> ', props)
-  // const {location:{search}} = props;
-  // const {type} = qs.parse(search.slice(1))
+const IndexRefund: FC<IndexRefundType> = ({orderInfo}) => {
+  
+  useEffect(() => {
+    // const {goodsName} = orderInfo
+    console.log('objectpropspropspropsprops :>> ', orderInfo);
+  }, [])
+  // const {goodsName,travelStartDate,travelEndDate,adultNum,childNum,promotionalImageUrl} = props.orderInfo
   return (
     <div className="refund-container">
       <div className="refund-main">
         <div className="refund-card">
-          <GoodsCard />
+          {/* <GoodsCard
+            goodsName={goodsName}
+            startDate={travelStartDate}
+            endDate={travelEndDate}
+            adultNum={adultNum}
+            childNum={childNum}
+            promotionalImageUrl={promotionalImageUrl}
+          /> */}
           <PreferCard />
         </div>
         <ApplyRefundCard />
