96de3512b6c0bc4e4adad4da0d086253b2ff9c25
wangshaohui <wangshaohui@openseas.com.cn>
2022-01-12 10:59:59 +0800

    顶部适配

--- src/components/myNavBar/index.tsx
+++ src/components/myNavBar/index.tsx
@@ -1,11 +1,15 @@
+import { isApp } from '@/jsbridge/env'
 import React, { useEffect, useState } from 'react'
 import { FC } from 'react'
 import { NavBar } from 'react-vant'
 import { NavBarProps } from 'react-vant/es/nav-bar'
 import './index.less'
+import Cookies from 'js-cookie'
+import { AppBridge } from '@/jsbridge/app'
 const MyNavBar: FC<NavBarProps> = (props: NavBarProps) => {
   let status = 0 // 0:还没数据，-1:不支持，1:支持
   const [safaArea, setSafeArea] = useState(true)
+  const [safeAreaTopValue, setSafeAreaTopValue] = useState(0)
   /**
    * 判断当前设置是否支持constant(safe-area-inset-top)或env(safe-area-inset-top)；
    * 部分Android设备，可以认识safa-area-inset-top，但会将其识别为0
@@ -14,11 +18,26 @@ const MyNavBar: FC<NavBarProps> = (props: NavBarProps) => {
 
   useEffect(() => {
     if (props.safeAreaInsetTop) {
-      setTimeout(() => {
-        setSafeArea(supportSafeArea())
-      }, 0)
+      if (isApp()) {
+        setTimeout(() => {
+          getAppAreaTop()
+        }, 0)
+      } else {
+        setTimeout(() => {
+          setSafeArea(supportSafeArea())
+        }, 0)
+      }
     }
   }, [])
+
+  const getAppAreaTop = () => {
+    const flutterWidth = Cookies.get('app_clientWidth')
+    const safeAreaTop = Cookies.get('safeAreaTop')
+    AppBridge.showToast(flutterWidth || '')
+    const width = document.body.clientWidth
+    setSafeAreaTopValue(Number(safeAreaTop) / (Number(flutterWidth) / width))
+  }
+
   const supportSafeArea = (): boolean => {
     if (status !== 0) {
       // 缓存数据，只向 body 插入一次 dom 即可
@@ -57,6 +76,7 @@ const MyNavBar: FC<NavBarProps> = (props: NavBarProps) => {
       className={classNames('MyNavBar', {
         'MyNavBar-top': !safaArea, // 不支持时，需要额外设置属性
       })}
+      style={safeAreaTopValue > 0 ? { paddingTop: safeAreaTopValue + 'px' } : {}}
     >
       <NavBar
         title={props.title}
