cbcf8ae518ac0701a3861d123ca4b64ad222e59f
fangmingdong <fangmingdong0224@gmail.com>
2022-01-15 21:41:17 +0800

    fix: Auto commit by fe-cli.

--- src/pages/testWXPage/index.tsx
+++ src/pages/testWXPage/index.tsx
@@ -106,6 +106,20 @@ const TestWXPage = () => {
           }}
         />
       </Cell.Group>
+
+      <Cell
+        title="chooseImage"
+        onClick={() => {
+          window['wx'].chooseImage({
+            count: 1, // 默认9
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+              console.log(res);
+            },
+          })
+        }}
+      />
     </div>
   )
 }
