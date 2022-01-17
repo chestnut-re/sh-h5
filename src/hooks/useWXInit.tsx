c53a87fae30e1dd488f9534be5b2a6d5429bd39f
fangmingdong <fangmingdong0224@gmail.com>
2022-01-17 12:54:29 +0800

    fix: Auto commit by fe-cli.

--- src/hooks/useWXInit.tsx
+++ src/hooks/useWXInit.tsx
@@ -34,7 +34,7 @@ export default function useWXInit(): any {
       console.log('wx error', res)
     })
 
-    WXService.getSignature(`${window.location.origin}${window.location.pathname}`).then((res) => {
+    WXService.getSignature(`${window.location.href}`).then((res) => {
       console.log(res)
       window['wx'].config({
         debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
