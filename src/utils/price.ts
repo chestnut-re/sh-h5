c6eda59ffbea24549204a642cb44a90b6b43f414
wangshaohui <wangshaohui@openseas.com.cn>
2022-01-20 17:19:52 +0800

    修改价格

--- src/utils/price.ts
+++ src/utils/price.ts
@@ -1,3 +1,10 @@
+/**
+ *
+ * @param price number | string
+ * @param toFixed number
+ * @returns number
+ * 格式化价格方法
+ */
 function getPrice(price: number | string, toFixed = 0): number {
   if (price == '' || price == null) {
     return 0
