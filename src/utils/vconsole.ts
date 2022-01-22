c09b89a9c020f8ea8dbaa486bb28c4d4f2428af8
fangmingdong <fangmingdong0224@gmail.com>
2022-01-22 14:22:16 +0800

    fix: vconsole

--- src/utils/vconsole.ts
+++ src/utils/vconsole.ts
@@ -1,5 +1,4 @@
 import Vconsole from 'vconsole'
 const isEnvProduction = process.env.NODE_ENV === 'production'
-// const vConsole = isEnvProduction ? null : new Vconsole()
-const vConsole = null
+const vConsole = isEnvProduction ? null : new Vconsole()
 export default vConsole
