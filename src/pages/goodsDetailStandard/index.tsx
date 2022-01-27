8f99162f476c3881cca17cec2a605ed400ac97da
fangmingdong <fangmingdong0224@gmail.com>
2022-01-27 13:59:30 +0800

    fix: Auto commit by fe-cli.

--- src/pages/goodsDetailStandard/index.tsx
+++ src/pages/goodsDetailStandard/index.tsx
@@ -18,9 +18,18 @@ const GoodsDetailStandardPage: React.FC = () => {
         <div className="content">导游：全程导游服务</div>
         <div className="content">保险：旅行社责任险</div>
         <div className="title">购物：</div>
-        <div className="content">0购物 0自费</div>
+        <div className="content">0购物</div>
+        <div className="title">自费：</div>
+        <div className="content">0自费</div>
         <div className="title">报价不含：</div>
         <div className="content">客人自愿选择参加的额外消费和私人消费、自由活动期 间的餐饭、车费等</div>
+        <div className="title">报价不含：</div>
+        <div className="content">客人自愿选择参加的额外消费和私人消费、自由活动期 间的餐饭、车费等</div>
+        <div className="content">个人消费(如酒店内洗衣、电话及未提到的其它服务)</div>
+        <div className="content">单间差或加床费用、酒店押金(须由客人现付酒店前台)</div>
+        <div className="content">
+          因天气原因、交通延误、取消等意外事件或战争、罢工、交通 事故、自然灾害等不可抗拒力导致的额外费用需自理
+        </div>
       </div>
     </div>
   )
